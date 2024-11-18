abd4bdc3a5b9f29f0b8f96df2bf8135fcb301ad6
geanmarcus <geanmarcus@gmail.com>
2024-11-18 00:11:06 -0300

    Listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <div>Next E-commerce 15 </div>
+    <div className="border max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 border sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+
+      </div>
+      
+    </div>
   )
 }
